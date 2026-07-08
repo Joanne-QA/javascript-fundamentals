194cb71d6c6fd91ab4c913413611007586d6d4ec
Joanne-QA <128373940+Joanne-QA@users.noreply.github.com>
2026-07-08 12:55:22 +0100

    Add variables fundamentals examples
    Added my first JavaScript variables practice file.

---
+++ variables.js
@@ -0,0 +1,22 @@
+// ===========================================
+// JavaScript Fundamentals
+// Topic: Variables
+// Author: Joanna Idowu
+// ===========================================
+
+// String
+let firstName = "Joanna";
+
+// Number
+let yearsOfExperience = 3;
+
+// Boolean
+let isLearningJavaScript = true;
+
+// Constant
+const jobTitle = "Regional QA Engineer";
+
+console.log(firstName);
+console.log(yearsOfExperience);
+console.log(isLearningJavaScript);
+console.log(jobTitle);
