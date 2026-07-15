3dc0c9925ba15928692648158a201a4124adfb11
Joanne-QA <128373940+Joanne-QA@users.noreply.github.com>
2026-07-15 10:43:32 +0100

    Create let-vs-const.js

---
+++ let-vs-const.js
@@ -0,0 +1,19 @@
+// ===========================================
+// JavaScript Fundamentals
+// Topic: let vs const
+// Author: Joanna Idowu
+// ===========================================
+
+// let exmaple
+let score = 10;
+
+console.log("Initial score", score);
+
+score = 20;
+
+console.log("Updated score:", score);
+
+// const example
+const company = "Jumia Africa";
+
+console.log("Company:", company); 
